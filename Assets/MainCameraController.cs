2d5d7a8fd36760545d2f757fd2b12ddac6bad4f3
Nyllise Graham <blu@udel.edu>
2025-02-08 16:51:01 -0500

    Finishing environment setup. Added MainCameraController.

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("I like my CompTIA hot and full of security certs.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
